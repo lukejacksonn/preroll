3a88e2e44888edf71645f5126e9b2ccfbc6342f1
Luke Jackson <lukejacksonn@gmail.com>
2017-10-01 20:57:45 +0100

    Adds css-modules autoprefixer and cssnano

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+// Scripts
 var buble = require('rollup-plugin-buble')
 var commonjs = require('rollup-plugin-commonjs')
 var resolve = require('rollup-plugin-node-resolve')
 var uglify = require('rollup-plugin-uglify')
 var serve = require('rollup-plugin-serve')
 var livereload = require('rollup-plugin-livereload')
+// Styles
+var postcssModules = require('postcss-modules')
 var postcss = require('rollup-plugin-postcss')
 var nested = require('postcss-nested')
+var cssnano = require('cssnano')
+var autoprefixer = require('autoprefixer')
+
+var cssExportMap = {}
 
 module.exports = function(dev) {
   return [
-    postcss({ plugins: [nested()] }),
+    postcss({
+      plugins: [
+        nested(),
+        postcssModules({
+          getJSON (id, exportTokens) {
+            cssExportMap[id] = exportTokens;
+          }
+        }),
+      ].concat(dev ? [] : [
+        autoprefixer('last 2 versions'),
+        cssnano(),
+      ]),
+      getExportNamed: false,
+      getExport (id) {
+        return cssExportMap[id];
+      }
+    }),
     resolve({ jsnext: true }),
     commonjs(),
     buble({ jsx: 'h' }),
